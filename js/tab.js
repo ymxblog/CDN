191784626d086c56992bf6d6d0f7873e1d5416d7
ymxblog <ymxblog@qq.com>
2025-03-24 00:53:27 +0800

    Create tab.js

---
+++ js/tab.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // Action buttons
+    $('.actionButton').on('click', function() {
+        var url = $(this).data('url'); // 获取按钮的 URL
+        var newTab = $(this).data('newtab'); // 判断是否在新标签页打开
+
+        if (newTab) {
+            // 如果需要在新标签页打开链接
+            window.open(url, '_blank');
+        } else {
+            // 否则使用 PJAX 加载内容
+            $.pjax({
+                url: url,
+                container: '#pjax-container'
+            });
+        }
+    });
+
+    // 可选：处理 PJAX 事件
+    $(document).on('pjax:end', function() {
+        console.log('PJAX 加载完成');
+    });
+});
